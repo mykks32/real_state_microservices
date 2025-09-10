88db25d4db2afb28e217a9f1afe7c1c02df6a2a3
mykks32 <srikri5462ydv@gmail.com>
2025-09-10 18:41:00 +0545

    feat: deleteing nessary

--- user-service/src/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { JwtService } from './jwt.service';
-
-@Module({
-  providers: [AuthService, JwtService],
-  exports: [JwtService]
-})
-export class AuthModule { }
