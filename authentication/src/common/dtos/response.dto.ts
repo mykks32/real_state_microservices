842c0ed4ef19b5bf192a517299c6169eee5691bd
mykks32 <srikri5462ydv@gmail.com>
2025-09-12 12:30:26 +0545

    feat: common request & response dto added

---
+++ authentication/src/common/dtos/response.dto.ts
@@ -0,0 +1,38 @@
+class ApiResponse<T> {
+  success: boolean;
+  message: string;
+  statusCode: number;
+  data?: T;
+  timestamp: string;
+  requestId?: string;
+
+  constructor(partial: Partial<ApiResponse<T>>) {
+    Object.assign(this, partial);
+  }
+
+  static ok<T>(
+    data: T,
+    message = 'success',
+    statusCode = 200,
+    requestId?: string,
+  ) {
+    return new ApiResponse<T>({
+      success: true,
+      statusCode,
+      message,
+      data,
+      requestId,
+      timestamp: new Date().toISOString(),
+    });
+  }
+
+  static error(message = 'Error', statusCode = 500, requestId?: string) {
+    return new ApiResponse<null>({
+      success: false,
+      statusCode,
+      message,
+      requestId,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
