70e1d6cd04428cb2e835c3512215b7dbd46d8b77
mykks32 <srikri5462ydv@gmail.com>
2025-09-20 19:34:36 +0545

    fix: location docs updated - docs

--- property-service/src/main/java/com/realState/property_service/module/location/dto/LocationDTO.java
+++ property-service/src/main/java/com/realState/property_service/module/location/dto/LocationDTO.java
@@ -2,13 +2,11 @@ package com.realState.property_service.module.location.dto;
 
 import com.realState.property_service.database.enums.StateEnum;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+/**
+ * DTO representing Location details.
+ */
 public class LocationDTO {
+
     private Long id;
     private String address;
     private String city;
@@ -17,55 +15,69 @@ public class LocationDTO {
     private Integer zipcode;
     private Float latitude;
     private Float longitude;
-    
+
+    // Getters and Setters
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public StateEnum getState() {
         return state;
     }
+
     public void setState(StateEnum state) {
         this.state = state;
     }
+
     public String getCountry() {
         return country;
     }
+
     public void setCountry(String country) {
         this.country = country;
     }
+
     public Integer getZipcode() {
         return zipcode;
     }
+
     public void setZipcode(Integer zipcode) {
         this.zipcode = zipcode;
     }
+
     public Float getLatitude() {
         return latitude;
     }
+
     public void setLatitude(Float latitude) {
         this.latitude = latitude;
     }
+
     public Float getLongitude() {
         return longitude;
     }
+
     public void setLongitude(Float longitude) {
         this.longitude = longitude;
     }
-
-    
 }
--- property-service/src/main/java/com/realState/property_service/module/location/dto/UpdateLocationDTO.java
+++ property-service/src/main/java/com/realState/property_service/module/location/dto/UpdateLocationDTO.java
@@ -1,15 +1,13 @@
 package com.realState.property_service.module.location.dto;
 
 import com.realState.property_service.database.enums.StateEnum;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+/**
+ * DTO for updating Location details.
+ * All fields are optional for partial updates.
+ */
 public class UpdateLocationDTO {
 
-    // All fields are nullable, meaning optional for update
     private String address;
     private String city;
     private StateEnum state;
@@ -18,6 +16,7 @@ public class UpdateLocationDTO {
     private Float latitude;
     private Float longitude;
 
+    // Getters and Setters
     public String getAddress() {
         return address;
     }
@@ -73,5 +72,4 @@ public class UpdateLocationDTO {
     public void setLongitude(Float longitude) {
         this.longitude = longitude;
     }
-
 }
