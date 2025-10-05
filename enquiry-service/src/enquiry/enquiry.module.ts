5a46b9e8000061a3209c1544007bd3330ed7c626
mykks32 <srikri5462ydv@gmail.com>
2025-10-05 13:40:12 +0545

    fix(enquiry): file import of entity fixed

--- enquiry-service/src/enquiry/enquiry.module.ts
+++ enquiry-service/src/enquiry/enquiry.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { EnquiryController } from './enquiry.controller';
 import { EnquiryService } from './enquiry.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Enquiry } from './enquiry.entity';
+import { Enquiry } from 'src/database/entities/enquiry.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Enquiry])],
