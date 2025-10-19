176badb9e54079a39bb0e87a385e7b2e214fce6a
mykks32 <srikri5462ydv@gmail.com>
2025-10-19 14:08:22 +0545

    feat(landing): initial commit

---
+++ landing-frontend/app/layout.tsx
@@ -0,0 +1,32 @@
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
+import "./globals.css";
+import React from "react";
+
+const inter = Inter({
+    variable: "--font-inter",
+    subsets: ["latin"],
+    display: "swap",
+    axes: ["opsz"],
+});
+
+export const metadata: Metadata = {
+    title: "Real State Application",
+    description: "Find real state in nepal.",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+        <body
+            className={`${inter.variable} font-sans antialiased bg-neutral-950 text-white`}
+        >
+        {children}
+        </body>
+        </html>
+    );
+}
