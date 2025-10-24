d062dc9d1eb97f216f888f7bedd03241f4cb6c10
mykks32 <srikri5462ydv@gmail.com>
2025-10-24 20:16:14 +0545

    refactor(api-gateway/property): property swagger updated

--- property-service/src/main/java/com/realState/property_service/database/repository/LocationRepository.java
+++ property-service/src/main/java/com/realState/property_service/database/repository/LocationRepository.java
@@ -1,11 +1,12 @@
 package com.realState.property_service.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.realState.property_service.database.entity.Location;
 
 @Repository
-public interface LocationRepository extends JpaRepository<Location, Long> {
+public interface LocationRepository extends JpaRepository<Location, Long>, JpaSpecificationExecutor<Location> {
 
 }
